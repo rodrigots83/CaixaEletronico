1c3b1f4a8f079c6be8f5fdc76af225e4d6a1aeaf
Rodrigo Teixeira dos Santos <diguu83@hotmail.com>
2018-07-02 17:56:54 -0300

    Atualização de arquivos.

--- CaixaEletronico/Titular.cs
+++ CaixaEletronico/Titular.cs
@@ -8,7 +8,28 @@ namespace CaixaEletronico
 {
     class Titular
     {
+        public int Idade { get; set; }
         public string Cpf { get; set; }
-        public string Nome { get; set; }        
+        public string Nome { get; set; }
+        public bool EEmancipado { get; set; }
+
+        public Titular()
+        {
+
+        }
+
+        public Titular(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public bool PodeAbrirConta()
+        {
+            var maiorDeIdade = (this.Idade >= 18);
+            var emancipado = (this.EEmancipado);
+            var temCpf = !string.IsNullOrEmpty(this.Cpf);
+
+            return (maiorDeIdade || emancipado) && temCpf;
+        }
     }
 }
